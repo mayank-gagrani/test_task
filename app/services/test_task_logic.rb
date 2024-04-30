59eadfa992dc4681cdf898706d953acd1e9b3023
Mayank Gagrani <m.gagrani01@gmail.com>
2024-04-30 11:04:29 +0530

    tdd task for add string

--- app/services/test_task_logic.rb
+++ app/services/test_task_logic.rb
@@ -1,5 +1,7 @@
 class TestTaskLogic
-  def self.add(numbers_string)
-    return 0 if numbers_string.empty?
+  def self.add(num_string)
+    return 0 if num_string.empty?
+    numbers = num_string.split(',').map(&:to_i) # sum of number in string
+    numbers.sum
   end
 end
--- spec/services/test_task_logic_spec.rb
+++ spec/services/test_task_logic_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-require 'test_task_logic' 
+# require 'test_task_logic' 
 
 
 RSpec.describe TestTaskLogic do
@@ -8,6 +8,27 @@ RSpec.describe TestTaskLogic do
        result = TestTaskLogic.add("")
        expect(result).to eq(0)
        puts "returns 0 for an empty string"
+     end
+
+     it 'returns sum for an string' do
+       result = TestTaskLogic.add("1")
+       expect(result).to eq(1)
+       puts "returns 1 for an empty string"
+     end
+
+     it 'returns sum for an string' do
+       result = TestTaskLogic.add("0,5")
+       expect(result).to eq(5)
+     end
+
+     it 'returns 11 for an string' do
+       result = TestTaskLogic.add("3,2,6")
+       expect(result).to eq(11)
+     end
+
+     it 'returns 12 for an empty string' do
+       result = TestTaskLogic.add("1,4,2,5")
+       expect(result).to eq(12)
      end
   end
 end
